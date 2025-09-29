e219d187c6855fa7a85a9ccb0dea1d14c34c0b63
Cuong <tricuongptdtntsd@gmail.com>
2025-09-29 22:06:47 +0700

    Update

--- src/main/java/com/n11/sportshop/SportshopApplication.java
+++ src/main/java/com/n11/sportshop/SportshopApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SportshopApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		
 		SpringApplication.run(SportshopApplication.class, args);
 	}
 
